9e9aad0ccf52073421f058ad36d18a21bdf46c17
AbdElRahman Rafaat Amer <3bdorafaat@gmail.com>
2022-06-05 23:01:23 +0200

    Revert "finish Ui & retrieve currency"
    This reverts commit bde71e88135742b2579f9d777ff044e76c46b3e9.

--- app/src/main/java/com/example/clickbuy/network/RemoteSource.kt
+++ app/src/main/java/com/example/clickbuy/network/RemoteSource.kt
@@ -5,7 +5,6 @@ import retrofit2.Response
 
 interface RemoteSource {
 
-<<<<<<< Updated upstream
     suspend fun getAllProducts():  Response<Products>
     suspend fun getAllProductsInCollectionByID(collectionID: String):Response<Products>
     suspend fun getCustomCollectionsByID(collectionID: String): Response<CustomCollectionElement>
@@ -14,24 +13,4 @@ interface RemoteSource {
     suspend fun getAvailableCoupons():  Response<Coupon>
     suspend fun getAvailableAds():  Response<Ads>
     suspend fun getAllSubCategoriesForSpecificCategory(collectionID: String): Response<Products>
-=======
-    suspend fun getAllProducts(): Response<Products>
-    suspend fun getAllProductsInCollectionByID(collectionID: String): Response<Products>
-    suspend fun getProductByID(productId: String): Response<ProductParent>
-    //suspend fun getCustomCollectionsByID(collectionID: String): Response<CustomCollectionElement>
-
-    suspend fun getCategoryIdByTitle(categoryTitle: String): Response<CustomCollections>
-    suspend fun getAllBrands(): Response<Brands>
-
-    // suspend fun getAllSubCategoriesForSpecificCategory(collectionID: String): Response<Products>
-    suspend fun getAllProductsInSpecificCollectionByIDAndTitle(
-        idCollectionDetails: String,
-        categoryTitleComingFromHome: String
-    ): Response<Products>
-//  suspend fun getCustomCollectionsByID(collectionID: String): Response<CustomCollectionElement>
-
-
-    suspend fun getCustomerDetails(email: String): Response<Customers>
-    suspend fun getCurrencies(): Response<Currencies>
->>>>>>> Stashed changes
 }
--- app/src/main/java/com/example/clickbuy/network/RetrofitClient.kt
+++ app/src/main/java/com/example/clickbuy/network/RetrofitClient.kt
@@ -39,7 +39,6 @@ class RetrofitClient : RemoteSource {
         return response
     }
 
-<<<<<<< Updated upstream
     override suspend fun getAllCustomCollections(): Response<CustomCollections> {
         var response = retrofitHelper.getAllCustomCollections()
         Log.i(TAG, "getAllCustomCollections: responseCode ---->\n ${response.code()}")
@@ -49,80 +48,13 @@ class RetrofitClient : RemoteSource {
 
     override suspend fun getAvailableCoupons(): Response<Coupon> {
         TODO("Not yet implemented")
-=======
-//    override suspend fun getCustomCollectionsByID(collectionID: String) {
-//        var response = retrofitHelper.getCategoryIdByTitle(collectionID)
-//        Log.i(TAG, "getCustomCollectionsByID: responseCode ---->\n ${response.code()}")
-//        Log.i(TAG, "getCustomCollectionsByID: response ---->\n ${response.body()}")
-
-    override suspend fun getCategoryIdByTitle(categoryTitle: String): Response<CustomCollections> {
-        var response = retrofitHelper.getCategoryIdByTitle(categoryTitle)
-        Log.i(TAG, "getCategoryIdByTitle: " + response.code())
-
-        return response
-    }
-
-//
-//        override suspend fun getAllCustomCollections(): Response<CustomCollections> {
-//            var response = retrofitHelper.getAllCustomCollections()
-//            Log.i(TAG, "getAllCustomCollections: responseCode ---->\n ${response.code()}")
-//            Log.i(TAG, "getAllCustomCollections: response ---->\n ${response.body()}")
-//            return response
-//        }
-
-
-//        override suspend fun getAllSubCategoriesForSpecificCategory(collectionID: String): Response<Products> {
-//            var response = retrofitHelper.getAllSubCategoriesForSpecificCategory(collectionID)
-//            Log.i(
-//                TAG,
-//                "getCustomCollectionsByID: responseCode ---->\n ${response.code()}"
-//            )
-//            Log.i(
-//                TAG,
-//                "getCustomCollectionsByID: response ---->\n ${response.body()}"
-//            )
-//            return response
-//        }
-
-
-    override suspend fun getAllBrands(): Response<Brands> {
-        var response = retrofitHelper.getAllBrands()
-        Log.i(TAG, "getAllBrands: ${response.body()}")
-        return response
->>>>>>> Stashed changes
     }
 
     override suspend fun getAvailableAds(): Response<Ads> {
         TODO("Not yet implemented")
     }
 
-<<<<<<< Updated upstream
     override suspend fun getAllSubCategoriesForSpecificCategory(collectionID: String): Response<Products> {
         TODO("Not yet implemented")
     }
 }
-=======
-    override suspend fun getAllProductsInSpecificCollectionByIDAndTitle(
-        idCollectionDetails: String,
-        categoryTitleComingFromHome: String
-    ): Response<Products> {
-        var response = retrofitHelper.getAllSubCategoriesForSpecificCategoryByIDAndTitle(
-            idCollectionDetails,
-            categoryTitleComingFromHome
-        )
-        return response
-    }
-
-    override suspend fun getCustomerDetails(email: String): Response<Customers> {
-        var response = retrofitHelper.getCustomerDetails(email)
-        Log.i(TAG, "getCustomerDetails: " + response.code())
-        return response
-    }
-
-    override suspend fun getCurrencies(): Response<Currencies> {
-        var response = retrofitHelper.getCurrencies()
-        Log.i(TAG, "getCurrencies: " + response.code())
-        return response
-    }
-}
->>>>>>> Stashed changes
